42dfc56dd04679913870c4c94f87e991066e9c4e
sondos alansi <sondos.alansi@yahoo.com>
2018-09-20 20:18:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file.
+
+function lowerCaseDrivers(drivers){
+  const lower=[];
+  for(const drive of drivers){
+    
+    lower.push(drive.toLowerCase());
+  }
+  return lower;
+}
+
+function nameToAttributes(drivers){
+  const names=[];
+  for(const drive of drivers){
+    
+   
+   Object.assign({}, drive, { firstName: '',lastName:'' });
+  }
+  return 
+}
